03fa89a7caee3843d9c5d195782335403d572cd4
Vitalii Andriiovskyi <domovik0712@gmail.com>
2020-04-01 21:02:59 +0300

    refactor: CounterButton2: make it ugly

--- src/components/CounterButton2.js
+++ src/components/CounterButton2.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class CounterButton2 extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      count: 0
-    
-  };
+      count: 0,
+    };
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-              if (this.state.count !== nextState.count) {
+    if (this.state.count !== nextState.count) {
       return true;
     }
     return false;
@@ -19,13 +19,14 @@ class CounterButton2 extends Component {
   render() {
     return (
       <button
-                 id='counter'
+        id="counter"
         color={this.props.color}
-        onClick={() => this.setState(state => ({count: state.count + 1}))}>
+        onClick={() => this.setState((state) => ({ count: state.count + 1 }))}
+      >
         Count: {this.state.count}
       </button>
     );
   }
 }
 
-export default CounterButton2
+export default CounterButton2;
